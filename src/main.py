5549217385aec9f988d987ef136634e51f0e373f
Tomdrs <hozda.tom@gmail.com>
2022-12-18 14:27:53 +0100

    Dummy rozpocet route

--- src/main.py
+++ src/main.py
@@ -8,6 +8,17 @@ def hello():
     index = str(open("frontend/index.html").read())
     return index
 
+@app.route('/rozpocet/<int:rok>')
+def rozpocet_na_rok(rok):
+    if rok == 2020:
+        return "23321847747474747"
+    elif rok == 2021:
+        return "2498743219982"
+    elif rok == 2022:
+        return "3"
+    else:
+        return "0"
+
 @app.route('/na_druhou/<int:cislo>')
 def ahoj(cislo):
     return str(cislo * cislo)
