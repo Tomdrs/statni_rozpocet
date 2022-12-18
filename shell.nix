68fd91a071d9dee31dbb9b41abb901ae44368ff6
Tomdrs <hozda.tom@gmail.com>
2022-12-18 13:46:25 +0100

    Activate virtualenv in nix-shell

--- shell.nix
+++ shell.nix
@@ -5,6 +5,11 @@ pkgs.mkShell {
         pkgs.coreutils-full
         pkgs.python3Full
         pkgs.python310Packages.flask
+        pkgs.python3.pkgs.virtualenv
         pkgs.pocketbase
     ];
+    shellHook = ''
+        source env/bin/activate
+    '';
+    FLASK_APP="src/main.py";
 }
