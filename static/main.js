d9196b4fa6d563f0cb5215363439f5c4a757d9cd
Tomdrs <hozda.tom@gmail.com>
2023-02-11 18:22:02 +0100

    add functions to main.js

--- static/main.js
+++ static/main.js
@@ -1,5 +1,21 @@
 // fetch rozpocet
 async function fetch_rozpocet(rok) {
     let req = await fetch("/rozpocet/" + rok);
-    return await req.text()
+    let rozpocet = await req.json();
+    console.log(rozpocet);
+    return rozpocet;
+}
+
+async function fetch_vydaje(rok) {
+    let req = await fetch("/vydaje/" + rok);
+    let vydaje = await req.json();
+    console.log(vydaje);
+    return vydaje;
+}
+
+async function fetch_prumerny_obcan() {
+    let req = await fetch("/prumerny_obcan");
+    let prumerny_obcan = await req.json();
+    console.log(prumerny_obcan);
+    return prumerny_obcan;
 }
