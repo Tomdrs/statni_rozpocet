3a37135b92db1b3c2d03cbba5cb7c6099d577b1f
Tomdrs <hozda.tom@gmail.com>
2023-03-19 12:01:00 +0100

    add waitress to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,18 +1,19 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
 pkgs.mkShell {
-    buildInputs = [
-        pkgs.coreutils-full
-        pkgs.python3Full
-        #pkgs.python310Packages.flask
-        pkgs.python3.pkgs.virtualenv
-        pkgs.pocketbase
-    ];
-    shellHook = ''
-        virtualenv -p python3 env
-        source env/bin/activate
-        pip install -r requirements.txt
-    '';
-    FLASK_APP="src/main.py";
-    POCKETBASE_URL="https://pocketbase.gjk.cat/";
+  buildInputs = [
+    pkgs.coreutils-full
+    pkgs.python3Full
+    #pkgs.python310Packages.flask
+    pkgs.python3.pkgs.virtualenv
+    pkgs.python310Packages.waitress
+    pkgs.pocketbase
+  ];
+  shellHook = ''
+    virtualenv -p python3 env
+    source env/bin/activate
+    pip install -r requirements.txt
+  '';
+  FLASK_APP = "src/main.py";
+  POCKETBASE_URL = "https://pocketbase.gjk.cat/";
 }
